a7acec1f22834bf94ff7ab798e67efad30d6df6e
João Pedro Zampoli <jpszampoli53@gmail.com>
2024-06-25 18:31:40 -0300

    Project Revival - Part 1

---
+++ meuportfolio/components/Layout.js
@@ -0,0 +1,9 @@
+import { motion } from "framer-motion";
+
+const Layout = ({ children }) => (
+    <motion.div initial={{ x: 0, opacity: 0 }} animate={{ x: 0, opacity: 1}} exit={{ x: 0, opacity: 0}} 
+    transition={{ease: "easeInOut", duration: 0.75}}>
+        {children}
+    </motion.div>
+);
+export default Layout;
---
+++ meuportfolio/pages/_app.js
@@ -0,0 +1,18 @@
+import '../styles/Global.css';
+import "../components/style.scss";
+import { AnimatePresence } from "framer-motion";
+import NavBar from '../components/NavBar';
+import Curve from '../components/Curve';
+
+export default function MyApp({ Component, pageProps, router }) {
+  return (
+    <>
+      <NavBar />
+        <AnimatePresence mode="wait">
+        <Curve backgroundColor="black" key={router.asPath}>
+          <Component {...pageProps} />
+          </Curve>
+        </AnimatePresence>
+    </>
+  );
+}
